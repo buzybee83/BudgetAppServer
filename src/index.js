406631239d5a327cd3bcd3faf358c24c74d0960a
Guivia Moreria <guivia.moreira@platform28.com>
2020-03-06 17:06:19 -0500

    Updated use model details & auth apis. Updated env db access keys & config

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('./models/User');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const keys = require('./config/keys');
 const authRoutes = require('./routes/authRoutes');
 const requireAuth = require('./middlewares/requireAuth');
 
@@ -10,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(authRoutes);
 
-const mongoUri = 'mongodb+srv://buzyb33:7YNVk489aRhqDk7m@cluster0-jufzm.mongodb.net/test?retryWrites=true&w=majority';
+const mongoUri = keys.mongoURI;
 mongoose.connect(mongoUri, {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -20,12 +21,25 @@ mongoose.connect(mongoUri, {
 mongoose.connection.on('connected', () => { console.log('Connected to mongo instance!'); });
 mongoose.connection.on('error', (err) => { console.error('Error  connecting to mongo: ', err); });
 
-
 app.get('/', requireAuth, (req, res) => {
     res.send(`Your email: ${req.user.email}`);
 });
 
-app.listen(3000, () => {
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', requireAuth, (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log('Listening on port 3000');
 });
 
